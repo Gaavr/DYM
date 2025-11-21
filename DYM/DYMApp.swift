188bc3b487c52cfad95052c4053be85ac5254416
Andrei Gavrilenko <gavrjob@gmail.com>
2025-11-21 19:50:54 +0300

    Initial Commit

---
+++ DYM/DYMApp.swift
@@ -0,0 +1,17 @@
+//
+//  DYMApp.swift
+//  DYM
+//
+//  Created by Andrei Gavrilenko on 21.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct DYMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
