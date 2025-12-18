7dd77a5cc8fc3117932d0805937ce3237391a7d3
Andrei Gavrilenko <gavrjob@gmail.com>
2025-12-18 23:56:35 +0300

    preparing for work

---
+++ DYM/Views/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  DYM
+//
+//  Created by Andrei Gavrilenko on 18.12.2025.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
