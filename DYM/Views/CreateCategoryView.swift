903d87fd3038bd530ed1de3c378abc2b4bddda65
Andrei Gavrilenko <gavrjob@gmail.com>
2026-01-28 01:05:52 +0300

    CategoryItemInListView

--- DYM/Views/CreateCategoryView.swift
+++ DYM/Views/CreateCategoryView.swift
@@ -9,24 +9,29 @@ import SwiftUI
 
 struct CreateCategoryView: View {
     
-    @State private var name = ""
-    @State private var categoryDescription = ""
+    @State private var name: String = ""
+    @State private var desctiption: String = ""
     @State private var color: Color = .blue
     @State private var icon: String = ""
     
     var body: some View {
         Form {
             Section {
+                CategoryItemInListView(name: name, description: desctiption, color: color, icon: icon)
+            }
+            Section {
                 TextField("Name", text: $name)
-                TextField("Description", text: $categoryDescription)
+                TextField("Description", text: $desctiption)
                 ColorPicker("Color", selection: $color)
                 TextField("Icon", text: $icon)
             }
             Section {
                 Button {
-                    
+                    print("Создаем категорию")
                 } label: {
                     Text("Create category")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .foregroundStyle(.black)
                 }
             }
         }
